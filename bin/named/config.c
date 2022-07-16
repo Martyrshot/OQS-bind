a728eea84236b9ca317a06043479fc6c8482e164
Jason Goertzen <Martyrshot@gmail.com>
2022-07-16 18:10:17 -0400

    More debugging

--- bin/named/config.c
+++ bin/named/config.c
@@ -83,7 +83,7 @@ options {\n\
 	memstatistics-file \"named.memstats\";\n\
 #	multiple-cnames <obsolete>;\n\
 #	named-xfer <obsolete>;\n\
-	nocookie-udp-size 4096;\n\
+	nocookie-udp-size 8192;\n\
 	notify-rate 20;\n\
 	nta-lifetime 3600;\n\
 	nta-recheck 300;\n\
