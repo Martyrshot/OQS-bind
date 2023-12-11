5c4a3f002c10500614e6052c6f2aa5af9022e016
Martyrshot <Martyrshot@gmail.com>
2023-12-11 11:16:27 -0600

    Fixed some typos in dsfromkey fix

--- bin/dnssec/dnssec-dsfromkey.c
+++ bin/dnssec/dnssec-dsfromkey.c
@@ -466,8 +466,8 @@ main(int argc, char **argv) {
 	}
 	printf("before Loading providers\n");
 #if OPENSSL_VERSION_NUMBER >= 0x30200000L && OPENSSL_API_LEVEL >= 30200
-	printf("Loading providers\N");
-	oqs = OSSL_PROVIDER_load(OSSL_LIB_CTX_get0_global_default(), "oqsprovider") {
+	printf("Loading providers\n");
+	oqs = OSSL_PROVIDER_load(OSSL_LIB_CTX_get0_global_default(), "oqsprovider");
 	if (oqs == NULL) {
 		ERR_print_errors_fp(stderr);
 		ERR_clear_error();
