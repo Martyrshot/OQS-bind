808ae8465493fee58d8a05803905daada2ba2021
jgoertzen-sb <jason.goertzen@sandboxquantum.com>
2023-12-13 11:34:37 -0600

    Enable NSEC3 for PQC

--- bin/dnssec/dnssec-keygen.c
+++ bin/dnssec/dnssec-keygen.c
@@ -367,6 +367,9 @@ keygen(keygen_ctx_t *ctx, isc_mem_t *mctx, int argc, char **argv) {
 			case DST_ALG_ECDSA384:
 			case DST_ALG_ED25519:
 			case DST_ALG_ED448:
+			case DST_ALG_FALCON512:
+			case DST_ALG_DILITHIUM2:
+			case DST_ALG_SPHINCSSHA256128S:
 				break;
 			default:
 				fatal("algorithm %s is incompatible with NSEC3"
