84bc2341cad55c134d7e3c141cff23647ac9f284
jgoertzen-sb <jason.goertzen@sandboxaq.com>
2023-12-15 13:38:10 -0600

    even more...

--- bin/named/main.c
+++ bin/named/main.c
@@ -1062,8 +1062,9 @@ create_managers(void) {
 
 // TODO remove after debugging
 int display_provider(OSSL_PROVIDER *provider, void *cbdata) {
-   printf("Provider loaded: %s\n", OSSL_PROVIDER_get0_name(provider));
-   return 1;
+	UNUSED(cbdata);
+	printf("Provider loaded: %s\n", OSSL_PROVIDER_get0_name(provider));
+	return 1;
 }
 
 
